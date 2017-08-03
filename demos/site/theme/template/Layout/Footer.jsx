2a84a14348cfecd5c68d6a5be80bf6710e770dce
walt <walt@agideo.com>
2017-08-03 12:19:05 +0800

    增加不蒜子

--- demos/site/theme/template/Layout/Footer.jsx
+++ demos/site/theme/template/Layout/Footer.jsx
@@ -7,9 +7,24 @@ class Footer extends React.Component {
   render() {
     return (
       <footer id="footer">
+        <div className="outer" style={{height: 10}}>
+          <div className="visit" style={{textAlign: "center"}}>
+            <span id="busuanzi_container_site_pv" style={{ display: "none" }}>
+              <span id="site-visit" >本站到访数:
+                <span id="busuanzi_value_site_uv"></span>
+              </span>
+            </span>
+            <span id="busuanzi_container_page_pv" style={{ display: "none" }}>
+              <span id="page-visit">, 本页阅读量:
+                <span id="busuanzi_value_page_pv"></span>
+              </span>
+            </span>
+          </div>
+        </div>
         <ul>
           <li>
             <h2><Icon type="github" /> GitHub</h2>
+
           </li>
           <li>
             <h2><Icon type="link" /> <FormattedMessage id="app.footer.links" /></h2>
